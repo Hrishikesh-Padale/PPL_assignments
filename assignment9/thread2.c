e71883774208363b5e2428211027763bc5866ad6
Hrishikesh-Padale <65584182+Hrishikesh-Padale@users.noreply.github.com>
2020-05-24 08:44:49 +0530

    Add files via upload

---
+++ assignment9/thread2.c
@@ -0,0 +1,45 @@
+#include <pthread.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+
+pthread_t thread[2]; 
+pthread_mutex_t lock; 
+
+void* myFunction(void* arg) {
+
+	pthread_mutex_lock(&lock); 
+	
+	int* val = (int *)arg;
+
+	if(*val == 0) {
+		printf("Hello World\n");
+	}
+	
+	else if (*val == 1) {
+		printf("Goodbye World\n");
+	}
+	
+	pthread_mutex_unlock(&lock); 
+	sleep(1);
+
+	return NULL; 
+} 
+
+int main() {
+ 
+	int i = 0;  
+
+	while(1) {
+		i = 0;
+		while (i < 2) { 
+			pthread_create(&(thread[i]), NULL, &myFunction, (void *)&i); 
+			pthread_join(thread[i], NULL); 
+			i++; 
+		} 
+	}
+	
+	pthread_mutex_destroy(&lock); 
+
+	return 0; 
+} 
